9d697c770e28efa4664d254236d9b87334102a01
bamp1 <34101960+bamp1@users.noreply.github.com>
2021-12-27 23:45:23 +0000

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.tp_pd_2122 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.tp_pd_2122 to javafx.fxml;
+    exports com.example.tp_pd_2122;
+}
